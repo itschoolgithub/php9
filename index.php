b95915add246b09f8bc5652b6e8a44207ef07725
itschool <itschool@tou.edu.kz>
2024-11-04 23:54:05 +0500

    init

---
+++ index.php
@@ -0,0 +1,187 @@
+<pre>
+    <?php
+        // $colors = ["red", "green", "blue"];
+        // print_r($colors);
+        // $colors = array_reverse($colors);
+        // print_r($colors);
+        // shuffle($colors);
+        // print_r($colors);
+
+        // $numbers = [2, 3, 1, 6, 7];
+        // print_r($numbers);
+
+        // sort($numbers);
+        // print_r($numbers);
+        
+        // rsort($numbers);
+        // print_r($numbers);  
+
+        // $marks = [
+        //     "max" => 100,
+        //     "john" => 75,
+        //     "james" => 80,
+        // ];
+        // print_r($marks);
+
+        // asort($marks);
+        // print_r($marks);
+        
+        // arsort($marks);
+        // print_r($marks);
+
+        // $person = [
+        //     "name" => "James",
+        //     "age" => 20,
+        //     "position" => "actor",
+        // ];
+        // print_r($person);
+
+        // ksort($person);
+        // print_r($person);
+        
+        // krsort($person);
+        // print_r($person);
+
+        // $fruits = ['apple', 'banana', 'pear', 'pineapple'];
+        // print_r($fruits);
+        // usort($fruits, 'mySort');
+        // print_r($fruits);
+        
+        // function mySort($a, $b) {
+        //     return strlen($a) - strlen($b);
+        // }
+
+        // usort($fruits, function ($a, $b) {
+        //     return strlen($a) - strlen($b);
+        // });
+        // print_r($fruits);
+
+        // $array = [948, 3884, 33];
+        // $array[] = 48488;
+        // print_r($array);
+        // array_push($array, 33, 45, 77);
+        // array_unshift($array, 1, 3, 4);
+        // print_r($array);
+        // $a = array_pop($array);
+        // $b = array_shift($array);
+        // print_r($array);
+
+        // $person = [
+        //     'name' => 'Erbol',
+        //     'age' => 20,
+        //     'city' => 'Almaty',
+        //     'country' => 'Kazakhstan',
+        // ];
+        // print_r($person);
+
+        // $values = array_values($person);
+        // print_r($values);
+
+        // $keys = array_keys($person);
+        // print_r($keys);
+
+        // $person = array_flip($person);
+        // print_r($person);
+
+        // $array = array_combine($keys, $values);
+        // print_r($array);
+
+        // $names = [
+        //     'Ethan',
+        //     'Olivia',
+        //     'Liam',
+        //     'Ava',
+        //     'Ethan',
+        //     'Olivia',
+        //     'Noah',
+        //     'Liam',
+        //     'Ethan',
+        // ];
+        // print_r($names);
+        // $names = array_unique($names);
+        // $names = array_values($names);
+        // print_r($names);
+        // $counts = array_count_values($names);
+        // print_r($counts); 
+        
+        // $data = [
+        //     'username' => 'john_doe',
+        //     'email' => 'john@example.com',
+        //     'age' => 25,
+        //     'country' => 'USA',
+        // ];
+
+        // print_r($data);
+        
+        // $newData = [
+        //     'email' => 'john.doe@gmail.com',
+        //     'age' => 26,
+        //     'city' => 'New York',
+        // ];
+        
+        // $updated = array_replace($data, $newData);
+        
+        // print_r($updated);
+
+        // $names1 = [
+        //     'Ethan',
+        //     'Olivia',
+        //     'Liam',
+        //     'Ava',
+        // ];
+        
+        // $names2 = [
+        //     'Ethan',
+        //     'Olivia',
+        //     'Noah',
+        //     'Liam',
+        //     'Ethan',
+        // ];
+        // $names = array_merge($names1, $names2);
+        
+        // print_r($names);
+
+        // $array = array_fill(1, 10, "+");
+        // print_r($array);
+
+        // $numbers = range(1, 10);
+        // print_r($numbers);
+
+        // $colors = [
+        //     'red', 'green', 'blue', 'yellow', 'black', 'white', 'orange', 'purple', 'brown',
+        //     'pink', 'gray', 'silver', 'gold',
+        // ];
+        // print_r($colors);
+        
+        // $someColors = array_slice($colors, 2, 5);
+        // print_r($someColors);
+
+        // $colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+        // print_r($colors);
+        // $newColors = ['purple',  'black', 'white', 'brown', 'pink'];
+        // print_r($newColors);
+        // array_splice($colors, 3, 1, $newColors);
+        // print_r($colors);
+
+        // $colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+        // $key = array_search('orange', $colors);
+        // var_dump($key);
+
+        // $products = [
+        //     'apple' => ['price' => 1.25, 'stock' => 100],
+        //     'banana' => ['price' => 0.75, 'stock' => 50],
+        //     'cherry' => ['price' => 2.00, 'stock' => 75],
+        //     'date' => ['price' => 1.50, 'stock' => 0],
+        // ];
+        // print_r($products);
+        // $inStockProducts = array_filter($products, 'filterInStock');
+        // $b = array_filter($product, function ($item) {
+        //     return true || false;
+        // });
+        // print_r($inStockProducts);
+        
+        // function filterInStock($product) {
+        //     return $product['stock'] > 0;
+        // }   
+    ?>
+</pre>
